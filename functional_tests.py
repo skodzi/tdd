177a4537af6f292a9b88e03223c0692791824f3b
Stanislaw Kodzis <Stanislaw.Kodzis@sabre.com>
2016-12-02 22:05:19 +0100

    Test funkcjonalny teraz sprawdza mozliwosc dodania elementu listy

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,21 +18,41 @@ class NewVisitorTest(unittest.TestCase):
 
         # zwrocila uwage, ze tytul strony i naglowek zawieraja slowo Listy
         self.assertIn('Listy', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy', header_text)
+        
+        # Od razu zostaje zachecona, aby wspisac rzecz do zrobienia
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Wpisz rzecz do zrobienia'
+            )
+        
+        # W polu tekstowym wpisala "Kupic pawie piora " (hobby Edyty
+        # polega na tworzeniu ozdobnych przynet
+        inputbox.send_keys('Kupic pawie piora')
+        
+        # Po nacisnieciu klawisza Enter strona zostala uaktualniona i wyslietla
+        # "1: Kupic pawie piora" jako element listy rzeczy do zrobienia
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find.element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Kupic pawie piora' for row in rows)
+            )
+        
+        # Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania
+        # Edyta wpisala "Uzyc pawich pior do zrobienia przyjety" (Edyta jest niezwykle skrupulatna)
         self.fail('Zakonczenie testu!')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
 
-# Od razu zostaje zachecona, aby wspisac rzecz do zrobienia
 
-# W polu tekstowym wpisala "Kupic pawie piora " (hobby Edyty
-# polega na tworzeniu ozdobnych przynet
 
-# Po nacisnieciu klawisza Enter strona zostala uaktualniona i wyslietla
-# "1: Kupic pawie piora" jako element listy rzeczy do zrobienia
 
-# Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania
-# Edyta wpisala "Uzyc pawich pior do zrobienia przyjety" (Edyta jest niezwykle skrupulatna)
+
+
+
+
 
 # Strona zostala ponownie uaktualniona i teraz wyswietla dwa elementy na liscie rzeczy do zrobienia
 
@@ -42,3 +63,5 @@ if __name__ == '__main__':
 
 # Usatysfakcjonowania kladzie sie spac
 
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
