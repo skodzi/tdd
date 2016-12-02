d057d6d923a9fe736d9c4849a9fcd80b69b7b18b
Stanislaw Kodzis <Stanislaw.Kodzis@sabre.com>
2016-12-02 21:38:04 +0100

    Prostywidok generuje terez minimalna ilosc kodu HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 #from django.test import TestCase
 
 # Create your tests here.
-#class SmokeTest(TestCase):
+# class SmokeTest(TestCase):
     
 #    def test_bad_maths(self):
 #        self.assertEqual(1 + 1, 3)
@@ -9,6 +9,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
     
@@ -16,3 +17,9 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
     
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Listy rzeczy do zrobienia</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
